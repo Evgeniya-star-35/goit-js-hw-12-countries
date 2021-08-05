a6fa3da4952b412cec468157f2d42043db22e044
Evgeniya Bayda <baje75128@gmail.com>
2021-08-05 22:49:28 +0300

    add style

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,6 +3,7 @@ function fetchCountries(name) {
   const SEARCH_PARAMS = 'fields=name;capital;population;flag;languages';
   return fetch(`${BASE_URL}${name}?${SEARCH_PARAMS}`).then(response => {
     if (response.ok) {
+      console.log(response);
       return response.json();
     }
     //пробрасываем ошибку, так как catch не всегда ловит ошибку 404
